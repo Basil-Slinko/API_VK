b4ff45785ca066685e17d3d215218b53974b3f9a
Basil-Slinko <86016038+Basil-Slinko@users.noreply.github.com>
2021-08-07 21:40:41 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import requests
-from pprint import pprint
-import os
 
 
 class YandexDisk:
@@ -18,9 +16,8 @@ class YandexDisk:
         headers = self.get_headers()
         name_folder = f'photo_vk_id{UserData.id_vk}'
         params = {"path": name_folder}
-        response = requests.put(url, headers=headers, params=params).json()
-        res = f'/{name_folder}/'
-        return res
+        requests.put(url, headers=headers, params=params)
+        return f'/{name_folder}/'
 
     def upload_file_to_ya_disk(self, name_file, url_file_path):
         href = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
@@ -31,13 +28,13 @@ class YandexDisk:
             'path': path,
             'url': url
             }
-        response = requests.post(href, headers=self.get_headers(), params=params)
-        res = response.json()
-        return res
+        response = requests.post(href, headers=self.get_headers(), params=params).json()
+        return response
 
 
 class VkUser:
     url = 'https://api.vk.com/method/'
+
     def __init__(self, token, version):
         self.params = {
             'access_token': token,
@@ -45,14 +42,13 @@ class VkUser:
             }
 
     def get_photos(self, count_photo=5):
-        self.count_photo = count_photo
         photos_get_url = self.url + 'photos.get'
         photos_get_params = {
             'owner_id': new_user_data.id_vk,
             'album_id': 'profile',
             'extended': 1,
             'photo_sizes': 1,
-            'count': self.count_photo
+            'count': count_photo
             }
         response = requests.get(photos_get_url, params={**self.params, **photos_get_params})
         if response.status_code != 200:
@@ -62,15 +58,15 @@ class VkUser:
         dict_photo_info = response.json()
         list_photo_info = dict_photo_info['response']['items']
 
-        dict_url_and_name_photo = {}
+        dict_url_and_name_photo = dict()
         for photo_info in list_photo_info:
             photo_sizes_list = photo_info['sizes']
             name_photo = photo_info['likes']['count']
             date_photo = photo_info['date']
 
-            dict_width_and_url_photo = {}
+            dict_width_and_url_photo = dict()
             for type_photo in photo_sizes_list:
-                list_type_and_url = []
+                list_type_and_url = list()
                 url_photo = type_photo['url']
                 width = type_photo['width']
                 type_photo = type_photo['type']
@@ -89,17 +85,16 @@ class VkUser:
 
         ya_disk_new_folder = YandexDisk(UserData.token_ya_disk)
         ya_disk_new_folder.create_a_folder()
-        log_json = []
+        log_json = list()
         for name_photo, url_photo in dict_url_and_name_photo.items():
             ya_disk_new_folder.upload_file_to_ya_disk(name_photo, url_photo[1])
-            info_about_file = {"file_name": None, "size": None}
-            info_about_file["file_name"] = f'{name_photo}.jpg'
-            info_about_file["size"] = url_photo[0]
-            log_json.append(info_about_file)
+            dict_info_about_file = dict(file_name=None, size=None)
+            dict_info_about_file["file_name"] = f'{name_photo}.jpg'
+            dict_info_about_file["size"] = url_photo[0]
+            log_json.append(dict_info_about_file)
 
         with open(f'photo_vk_id{UserData.id_vk}.json', 'a', encoding='utf-8') as file:
-            file.write(f'\nphoto_vk_id{UserData.id_vk}\n{log_json}')
-       #return log_json
+            file.write(f'{log_json}\n\n')
 
 
 class UserData:
@@ -110,4 +105,4 @@ class UserData:
 if __name__ == '__main__':
     vk_client = VkUser('958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008', '5.131')
     new_user_data = UserData
-    pprint(vk_client.get_photos())
+    vk_client.get_photos()
